cb23a5ddaa637d970ff396f6b42d607fdc721ec6
BROJ3 <toni.crnjak@hotmail.com>
2024-03-31 19:04:23 -0400

    create responsive grids

--- script.js
+++ script.js
@@ -12,53 +12,58 @@ function addElement() {
     if (parseInt(inputValue.value) >= 10 && parseInt(inputValue.value) <= 50) {
       console.log(listValue);
 
-        // loop to make 16 rows
-  for (let i = 0; i < inputValue.value; i++) {
 
-    let temporaryInput = 16;
-
-    //define each row
-    const row = document.createElement("div");
-    row.className = "row"
-    row.style.height = 500/parseInt(inputValue.value) + "px"
-    row.style.width = "500px"
-
-    //define flex
-    row.style.display = "flex"
-    row.style.flexWrap = "wrap"
-    row.style.flexDirection = "column"
-
-    //for each row, define 16 columns
-    for (let j = 0; j < inputValue.value; j++) {
-      const pixel = document.createElement("div")
-      pixel.className = "gridSquare"
-      //pixel.style.width = row.style.width/pixel.style.width;
-      pixel.style.height = row.style.height;
-      pixel.style.border = "1px solid black"
-
-      //append the column to row
-      row.appendChild(pixel)
+      document.removeChild(workingSpace); // OVAJJJJJ
 
 
-      // Add a mouseover event listener
-      pixel.addEventListener('mouseover', () => {
-        // Change the button's background color
-        pixel.style.backgroundColor = 'black';
-      });
+      // loop to make 16 rows
+      for (let i = 0; i < inputValue.value; i++) {
 
-      const clearButton = document.getElementById("clearMe");
-      clearButton.addEventListener('click', () => {
-        // Change the button's background color
-        pixel.style.backgroundColor = 'white';
-      });
+        let temporaryInput = 16;
+
+        //define each row
+        const row = document.createElement("div");
+        row.className = "row"
+        row.style.height = 500 / parseInt(inputValue.value) + "px"
+        row.style.width = "500px"
+
+        //define flex
+        row.style.display = "flex"
+        row.style.flexWrap = "wrap"
+        row.style.flexDirection = "column"
+
+        //for each row, define 16 columns
+        for (let j = 0; j < inputValue.value; j++) {
+
+          const pixel = document.createElement("div")
+          pixel.className = "gridSquare"
+          //pixel.style.width = row.style.width/pixel.style.width;
+          pixel.style.height = row.style.height;
+          pixel.style.border = "1px solid black"
+
+          //append the column to row
+          row.appendChild(pixel)
 
 
-    }
-    //create the row
+          // Add a mouseover event listener
+          pixel.addEventListener('mouseover', () => {
+            // Change the button's background color
+            pixel.style.backgroundColor = 'black';
+          });
 
-    workingSpace.appendChild(row);
+          const clearButton = document.getElementById("clearMe");
+          clearButton.addEventListener('click', () => {
+            // Change the button's background color
+            pixel.style.backgroundColor = 'white';
+          });
 
-  }
+
+        }
+        //create the row
+
+        workingSpace.appendChild(row);
+
+      }
 
     } else {
       alert("Select number between 10 and 50")
@@ -79,7 +84,7 @@ function addElement() {
     //define each row
     const row = document.createElement("div");
     row.className = "row"
-    row.style.height = 500/temporaryInput + "px"
+    row.style.height = 500 / temporaryInput + "px"
     row.style.width = "500px"
 
     //define flex
